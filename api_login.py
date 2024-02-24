39b098cecc21a66ecb6530abdfcb1129e143aedc
unknown <student@vstecb.local>
2024-02-24 14:25:52 +0100

    gets Token from Api and retrieves datai

--- api_login.py
+++ api_login.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def login(api_url, user_login, api_key):
+def getToken(api_url, user_login, api_key):
     data = {
         "Result": None,
         "UserLogin": user_login,
@@ -9,21 +9,50 @@ def login(api_url, user_login, api_key):
     }
     headers = {"Content-Type": "application/json"}
     response = requests.post(api_url, data=json.dumps(data), headers=headers)
-
     if response.status_code == 200:
         print("Login Successful")
-        return response.json()
+        return response.json().get("Token")
     else:
         print("Login Failed:", response.status_code, response.text)
         return None
-
-def main(user_login, api_key):
-    api_url = "http://dochazka.efg.cz/aktionnext/api/login"
-
-    token_info = login(api_url, user_login, api_key)
-    if token_info:
-        print("Token:", token_info.get("Token"))
-        print("Expires in:", token_info.get("ExpireInSec"), "seconds")
-
+def fetchFromApi(api_url: str, token: str, time_from: str, time_to: str):
+    params = {
+        "Token": token,
+        "TimeFrom": time_from,
+        "TimeTo": time_to
+    }
+    headers = {
+        "Accept": "application/json"
+    }
+    response = requests.get(api_url, headers=headers, params=params)
+    if response.status_code == 200:
+        print("Fetch Successful")
+        return response.json()
+    else:
+        print("Fetch Failed:", response.status_code, response.text)
+        return None
+def main(user_login: str, api_key: str):
+    token = getToken(
+        api_url="https://next.vstecb.cz/AktionNEXT/API/login",
+        user_login=user_login,
+        api_key=api_key
+    )
+    if token:
+        apiData = fetchFromApi(
+            api_url="https://next.vstecb.cz/AktionNEXT/API/attendance/getPassAll",
+            token=token,
+            time_from="2023-11-20T08:00:00",
+            time_to="2023-11-20T08:01:00"
+        )
+        print(apiData)
+    else:
+        print("Failed to retrieve token.")
+def loadApiLoginCredentials():
+    with open('apiLoginCredentials.json', 'r') as file:
+        credentials = json.load(file)
+    return credentials['user_login'], credentials['api_key']
 if __name__ == "__main__":
-    main(user_login="dochazka-ext", api_key="hDn/LFXIPESM036W+s7VKg==") ## priklad hesla z dokumentace {"Message":"UserLogin or ApiKey are unknown or user is blocked","Code":1003}
+
+    # Load credentials from JSON file
+    user_login, api_key = loadApiLoginCredentials()
+    main(user_login, api_key)
